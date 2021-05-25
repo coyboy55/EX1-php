34172c23f1ebd9ff95a093819dd285e553c13c50
coybyo55 <fakherddine.ab.hamda@gmail.com>
2021-05-25 17:19:11 +0300

    part 1 without css

---
+++ EX1-part1/register-login.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <link rel='stylesheet' href='style.css'/>
+    </head>
+<body class='body'>
+
+<form action="safe.php" method="post">
+fullname: <input type="text" name="name"><br>
+username: <input type="text" name="email"><br>
+
+password: <input type="password" name="pass1"><br>
+confirm password: <input type="password" name="pass2"><br>
+
+
+Email: <input type="email" name="Email"><br>
+Phone: <input type="phone" name="Phone"><br>
+
+
+Date of Birthpassword: <input type="text" name="Date"><br>
+Social Security Number: <input type="text" name="Social"><br>
+
+<input type="submit">
+</form>
+
+<form action="safe.php" method="post">
+username1: <input type="text" name="name1"><br>
+password: <input type="password" name="pass3"><br>
+
+
+
+<input type="submit">
+</form>
+
+</body>
+</html>
+
+
---
+++ EX1-part1/safe.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Welcome <?php echo $_POST["email"]; ?><br>
+
+
+pass1 <?php echo $_POST["pass1"]; ?><br>
+pass2 <?php echo $_POST["pass2"]; ?><br>
+
+
+Email <?php echo $_POST["Email"]; ?><br>
+Phone <?php echo $_POST["Phone"]; ?><br>
+
+Date <?php echo $_POST["Date"]; ?><br>
+Social <?php echo $_POST["Social"]; ?><br>
+
+
+username <?php echo $_POST["name1"]; ?><br>
+password <?php echo $_POST["passs3"]; ?><br>
+
+
+</body>
+</html>
