e21c32cd726c519ad23f331ea8b99b071a632827
coybyo55 <fakherddine.ab.hamda@gmail.com>
2021-05-25 18:06:21 +0300

    part4

---
+++ EX1-part4/output.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$arr = array("MUSICALS"=>array('Oklahoma','The Music Man','South Pacific'), 
+"DRAMAS"=>array('Lawrence of Arabia','To Kill a Mockingbird','Casablanca'), 
+"MYSTERIES"=>array('The Maltese Falcon','Rear Window','North by Northwest'));
+
+print_r($arr);
+
+
+ echo $arr['MUSICALS'];
+
+?>
+
+
+
+
