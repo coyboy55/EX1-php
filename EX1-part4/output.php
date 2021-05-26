06ea250490928a8b8e8a8eddc194b755962b811f
coybyo55 <fakherddine.ab.hamda@gmail.com>
2021-05-26 12:27:46 +0300

    part 4 done

--- EX1-part4/output.php
+++ EX1-part4/output.php
@@ -5,10 +5,19 @@ $arr = array("MUSICALS"=>array('Oklahoma','The Music Man','South Pacific'),
 "DRAMAS"=>array('Lawrence of Arabia','To Kill a Mockingbird','Casablanca'), 
 "MYSTERIES"=>array('The Maltese Falcon','Rear Window','North by Northwest'));
 
-print_r($arr);
 
 
- echo $arr[0];
+echo '<hr>';
+foreach ($arr as $key => $value) {
+   echo $key;
+echo '<br>';
+
+   foreach ($value as $key => $value) {
+echo '----> '.$key.' = '.$value;
+echo '<br>';
+   }
+}
+
 
 ?>
 
